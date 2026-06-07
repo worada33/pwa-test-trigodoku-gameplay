152c1b7909c77ca057ec11060a669f86035456a4
worada33 <woradaphunoi@gmail.com>
2026-06-07 13:08:00 +0700

    service-worker.js
    ทำให้เกบแคช และใช้ตอนออฟไลน์ได้

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,41 @@
-self.addEventListener('install', () => {
-  console.log('PWA Installed');
+const CACHE_NAME = 'trigodoku-v1';
+
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', () => {});
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
